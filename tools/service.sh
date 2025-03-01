01601f4cf29a54fef35d496cd4f005866b2c7d14
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2025-03-01 22:26:58 +0100

    Add custom dpi setter

--- tools/service.sh
+++ tools/service.sh
@@ -1,7 +1,14 @@
 (
+  # wait for boot to complete
   while [ "$(getprop sys.boot_completed)" != "1" ]; do
     sleep 1
   done
   sleep 3
   cmd overlay enable com.android.internal.systemui.navbar_spaced.gestural
-)& # wait for completing boot in background
+  # Wait for unlocking the device
+  until [ -d /sdcard/ ]; do
+    sleep 1
+  done
+  sleep 30
+  wm density 432
+)& # launch in background
